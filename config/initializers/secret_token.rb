b2143e418de57d8cda6062a572a6da38aa9cb264
Nick Edgington <NickEdgington@gmail.com>
2012-06-09 16:21:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryonrd::Application.config.secret_token = 'a5a45de53f891345eb80b68489b1fd50be30764ca64910d714abb91a70e1be534564bd304459044cc02d7bc3d9e4903e882d95e2887d5218f36be35448e7c9fe'
